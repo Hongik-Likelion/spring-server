0bf5fbfb2d71afa8f3a7ba39ef614d5edae41b13
HyunWoo <kimjd766@gmail.com>
2023-11-15 00:39:14 +0900

    feat:jpa auditing 설정

--- together-market/src/main/java/likelion/togethermarket/TogetherMarketApplication.java
+++ together-market/src/main/java/likelion/togethermarket/TogetherMarketApplication.java
@@ -2,8 +2,10 @@ package likelion.togethermarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TogetherMarketApplication {
 
 	public static void main(String[] args) {
