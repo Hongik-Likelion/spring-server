24cde34439cb0b8bd1e0584eb1a83d57212ec4ae
HyunWoo <kimjd766@gmail.com>
2023-11-14 16:32:07 +0900

    initial commit

---
+++ together-market/src/main/java/likelion/togethermarket/TogetherMarketApplication.java
@@ -0,0 +1,13 @@
+package likelion.togethermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherMarketApplication.class, args);
+	}
+
+}
---
+++ together-market/src/test/java/likelion/togethermarket/TogetherMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.togethermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
