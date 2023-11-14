31ba0b1ba0191d9b59513449e8d3a74dd92dc185
HyunWoo <kimjd766@gmail.com>
2023-11-14 22:30:42 +0900

    feat:Shop 엔티티 작성

---
+++ together-market/src/main/java/likelion/togethermarket/domain/shop/entity/Shop.java
@@ -0,0 +1,49 @@
+package likelion.togethermarket.domain.shop.entity;
+
+import jakarta.persistence.*;
+import likelion.togethermarket.domain.market.entity.Market;
+import likelion.togethermarket.domain.member.entity.Member;
+import likelion.togethermarket.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Shop extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shop_id")
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "market_id")
+    private Market market;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "shop_name", nullable = false)
+    private String shopName;
+
+    @Column(name = "shop_address")
+    private String shopAddress;
+
+    @Column(name = "selling_products")
+    private String sellingProducts;
+
+    @Column(name = "opening_time")
+    private String openingTime;
+
+    @Column(name = "closing_time")
+    private String closingTime;
+
+    @Column(name = "opening_frequency")
+    private String openingFrequency; // e.g. 매주, 매달, 격주 ...
+
+    @Column(name = "rating")
+    private Float rating;
+
+}
