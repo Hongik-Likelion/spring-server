d549fb59d4269ab29ae0b074d57da17f277d82c4
HyunWoo <kimjd766@gmail.com>
2023-11-17 16:21:03 +0900

    feat: singup request를 받기위한 DTO 작성

---
+++ together-market/src/main/java/likelion/togethermarket/domain/member/dto/SignupDto.java
@@ -0,0 +1,14 @@
+package likelion.togethermarket.domain.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+public class SignupDto {
+    private String email;
+    private String nickname;
+    private String profile;
+    private boolean is_owner; // python 에서 하던 프로젝트라 변수명이 흠...
+}
