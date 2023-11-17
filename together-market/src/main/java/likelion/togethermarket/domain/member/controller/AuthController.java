0a96250af3cdc3b7bf56a2258f14c5ed3bda217c
HyunWoo <kimjd766@gmail.com>
2023-11-17 16:21:44 +0900

    feat: singup api 매핑

---
+++ together-market/src/main/java/likelion/togethermarket/domain/member/controller/AuthController.java
@@ -0,0 +1,25 @@
+package likelion.togethermarket.domain.member.controller;
+
+import likelion.togethermarket.domain.member.dto.SignupDto;
+import likelion.togethermarket.domain.member.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class AuthController {
+
+    private final AuthService authService;
+
+    //login, reissue
+
+    //signup
+    public ResponseEntity<?> signup(@RequestBody SignupDto signupDto){
+        return authService.signupMember(signupDto);
+    }
+
+}
