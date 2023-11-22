9c03d114d21ddf110c28facc213987b2398a0ce0
HyunWoo <kimjd766@gmail.com>
2023-11-23 01:55:29 +0900

    feat:Member의 모든 wishMarket 삭제 추가

--- together-market/src/main/java/likelion/togethermarket/domain/market/repository/WishMarketRepository.java
+++ together-market/src/main/java/likelion/togethermarket/domain/market/repository/WishMarketRepository.java
@@ -1,10 +1,12 @@
 package likelion.togethermarket.domain.market.repository;
 
 import likelion.togethermarket.domain.market.entity.WishMarket;
+import likelion.togethermarket.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface WishMarketRepository extends JpaRepository<WishMarket, Long> {
     List<WishMarket> findAllByMember_Id(Long memberId);
+    void deleteAllByMember(Member member);
 }
