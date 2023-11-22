f871f81ff2e460aa50aaa25b8cadd4e6ba4e3cc3
HyunWoo <kimjd766@gmail.com>
2023-11-23 04:25:59 +0900

    feat:Member로 wishShop 모두 찾기 가능하게 설정

--- together-market/src/main/java/likelion/togethermarket/domain/shop/repository/WishShopRepository.java
+++ together-market/src/main/java/likelion/togethermarket/domain/shop/repository/WishShopRepository.java
@@ -5,8 +5,11 @@ import likelion.togethermarket.domain.shop.entity.Shop;
 import likelion.togethermarket.domain.shop.entity.WishShop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WishShopRepository extends JpaRepository<WishShop, Long> {
     Optional<WishShop> findByMemberAndShop(Member member, Shop shop);
+
+    List<WishShop> findAllByMember(Member member);
 }
