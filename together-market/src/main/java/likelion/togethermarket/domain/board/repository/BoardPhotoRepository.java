2410b6bbb149143e0527d82d53473e8feb0d1a10
HyunWoo <kimjd766@gmail.com>
2023-11-23 04:33:28 +0900

    feat:BoardPhoto 레포지토리 생성

--- together-market/src/main/java/likelion/togethermarket/domain/board/repository/BoardPhotoRepository.java
+++ together-market/src/main/java/likelion/togethermarket/domain/board/repository/BoardPhotoRepository.java
@@ -4,4 +4,5 @@ import likelion.togethermarket.domain.board.entity.BoardPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardPhotoRepository extends JpaRepository<BoardPhoto, Long> {
+
 }
