7c3003983687c0a5680b1175056cee4832ef30c5
HyunWoo <kimjd766@gmail.com>
2023-11-23 02:44:34 +0900

    feat:선호 시장 등록 메서드 개발

--- together-market/src/main/java/likelion/togethermarket/domain/market/service/MarketService.java
+++ together-market/src/main/java/likelion/togethermarket/domain/market/service/MarketService.java
@@ -2,8 +2,13 @@ package likelion.togethermarket.domain.market.service;
 
 import likelion.togethermarket.domain.market.dto.AllMarketDto;
 import likelion.togethermarket.domain.market.dto.BasicMarketDto;
+import likelion.togethermarket.domain.market.dto.WishMarketReqDto;
 import likelion.togethermarket.domain.market.entity.Market;
+import likelion.togethermarket.domain.market.entity.WishMarket;
 import likelion.togethermarket.domain.market.repository.MarketRepository;
+import likelion.togethermarket.domain.market.repository.WishMarketRepository;
+import likelion.togethermarket.domain.member.entity.Member;
+import likelion.togethermarket.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MarketService {
     private final MarketRepository marketRepository;
+    private final MemberRepository memberRepository;
+    private final WishMarketRepository wishMarketRepository;
 
     public ResponseEntity<?> getMarkets() {
         List<Market> markets = marketRepository.findAll();
@@ -25,4 +32,14 @@ public class MarketService {
         return new ResponseEntity<List<BasicMarketDto>>(marketDtos, HttpStatusCode.valueOf(200));
     }
 
+    public ResponseEntity<?> registerWishMarket(Long memberId, WishMarketReqDto reqDto) {
+        Member member = memberRepository.findById(memberId).orElseThrow();
+
+        for (Integer market_id : reqDto.getMarket_id()){
+            Market market = marketRepository.findById(Long.valueOf(market_id)).orElseThrow();
+            wishMarketRepository.save(WishMarket.builder().market(market).member(member).build());
+        }
+
+        return new ResponseEntity<>(HttpStatusCode.valueOf(201));
+    }
 }
