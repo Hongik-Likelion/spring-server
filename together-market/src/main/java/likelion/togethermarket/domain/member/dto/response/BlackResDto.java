aafa7ce365d983875ce6a5bda2aae996417ec510
HyunWoo <kimjd766@gmail.com>
2023-11-23 02:23:35 +0900

    feat:유저 차단 응답을 위한 DTO 개발

---
+++ together-market/src/main/java/likelion/togethermarket/domain/member/dto/response/BlackResDto.java
@@ -0,0 +1,26 @@
+package likelion.togethermarket.domain.member.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BlackResDto {
+    private int id;
+    private String email;
+    private String nickname;
+    private String profile;
+    private String introduction;
+    private boolean is_owner;
+
+    @Builder
+    public BlackResDto(int id, String email, String nickname, String profile, String introduction, boolean is_owner) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.profile = profile;
+        this.introduction = introduction;
+        this.is_owner = is_owner;
+    }
+}
