b9bdeb6451fb4ef861973c6a251d6352f8fa362d
HyunWoo <kimjd766@gmail.com>
2023-11-28 16:41:52 +0900

    feat: 내가 쓴 게시글 전체 조회를 위한 DTO 개발

---
+++ together-market/src/main/java/likelion/togethermarket/domain/board/dto/MyBoardInfoDto.java
@@ -0,0 +1,37 @@
+package likelion.togethermarket.domain.board.dto;
+
+import jakarta.annotation.Nullable;
+import likelion.togethermarket.domain.board.entity.Board;
+import likelion.togethermarket.domain.board.entity.BoardPhoto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class MyBoardInfoDto {
+
+    private int board_id;
+
+    private String updated_at;
+
+    @Nullable
+    private int rating;
+
+    @Nullable
+    private String photo;
+
+    private String content;
+
+    @Builder
+    public MyBoardInfoDto(Board board, List<BoardPhoto> boardPhotos) {
+        this.board_id = board.getId().intValue();
+        this.updated_at = board.getModifiedAt().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));;
+        this.rating = board.getRating();
+        this.photo = (boardPhotos.isEmpty())?null:boardPhotos.get(0).getImage();
+        this.content = board.getContent();
+    }
+}
